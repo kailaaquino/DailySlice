ae6ae9dafa0525bb18c1f3ad3b284bc235d2a711
Kaila Aquino <kailad.aquino@gmail.com>
2025-08-04 14:16:01 -0700

    Initial Commit

---
+++ DailySlice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailySlice
+//
+//  Created by Kaila Aquino on 8/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
